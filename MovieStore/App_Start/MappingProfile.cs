c1f2ed0f31dc31c3e2634833017379bcc912ca48
ih-hira <imtiyaz.aust@gmail.com>
2019-11-04 07:54:24 +0600

    commit 7: api controllers are added

---
+++ MovieStore/App_Start/MappingProfile.cs
@@ -0,0 +1,30 @@
+﻿using AutoMapper;
+using MovieStore.DTO;
+using MovieStore.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MovieStore.App_Start
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            // Domain to Dto
+            Mapper.CreateMap<Customer, CustomerDTO>();
+            Mapper.CreateMap<Movie, MovieDTO>();
+            Mapper.CreateMap<MembershipType, MembershipTypeDTO>();
+            Mapper.CreateMap<Genre, GenreDTO>().ForMember(g => g.Id, opt => opt.Ignore());
+
+
+            // Dto to Domain
+            Mapper.CreateMap<CustomerDTO, Customer>().ForMember(c => c.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<MovieDTO, Movie>().ForMember(m => m.Id, opt => opt.Ignore());
+
+            Mapper.CreateMap<GenreDTO, Genre>().ForMember(g => g.Id, opt => opt.Ignore());
+        }
+    }
+}
---
+++ MovieStore/Controllers/api/MoviesController.cs
@@ -0,0 +1,70 @@
+﻿using MovieStore.DTO;
+using MovieStore.Models;
+using System;
+using System.Data;
+using System.Data.Entity;
+using System.Data.Entity.Validation;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using AutoMapper;
+
+namespace MovieStore.Controllers.api
+{
+    public class MoviesController : ApiController
+    {
+        private ApplicationDbContext db;
+        public MoviesController()
+        {
+            db = new ApplicationDbContext();
+        }
+
+        //GET /api/movies
+        public IEnumerable<MovieDTO> GetMovies()
+        {
+            var moviesQuery = db.Movies.Include(m => m.Genre).Where(m => m.NumberInStock > 0);
+            return moviesQuery.ToList().Select(Mapper.Map<Movie, MovieDTO>);
+        }
+
+        //GET /api/movies/1
+        public IHttpActionResult GetMovie(int id)
+        {
+            var movie = db.Movies.SingleOrDefault(m => m.Id == id);
+            if (movie == null)
+                return NotFound();
+            return Ok(Mapper.Map<Movie, MovieDTO>(movie));
+        }
+
+        //POST /api/movies
+        [HttpPost]
+        public IHttpActionResult CreateMovie(MovieDTO movieDto)
+        {
+            if (!ModelState.IsValid)
+                return BadRequest();
+            var movie = Mapper.Map<MovieDTO, Movie>(movieDto);
+            db.Movies.Add(movie);
+            db.SaveChanges();
+
+            movieDto.Id = movie.Id;
+            return Created(new Uri(Request.RequestUri + "/" + movie.Id), movieDto);
+        }
+
+        //PUT /ap/customers/1
+        [HttpPut]
+        public IHttpActionResult UpdateMovie(int id, MovieDTO movieDto)
+        {
+            if (!ModelState.IsValid)
+                throw new HttpResponseException(HttpStatusCode.BadRequest);
+            var movieInDb = db.Movies.SingleOrDefault(m => m.Id == id);
+            if (movieInDb == null)
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+
+            Mapper.Map(movieDto, movieInDb);
+
+            db.SaveChanges();
+            return Ok();
+        }
+    }
+}
